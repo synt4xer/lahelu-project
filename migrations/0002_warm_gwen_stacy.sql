9f64fc8dda9a0bffa359fa576638ee8375a600c2
Ferrel John Fernando <f.syntaxer@gmail.com>
2024-12-18 01:14:56 +0700

    feat: update migration and comments schema

--- migrations/0002_warm_gwen_stacy.sql
+++ migrations/0002_warm_gwen_stacy.sql
@@ -9,6 +9,5 @@ CREATE TABLE "comments" (
 --> statement-breakpoint
 ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_posts_id_fk" FOREIGN KEY ("post_id") REFERENCES "public"."posts"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_user_id_posts_id_id_fk" FOREIGN KEY ("post_id","user_id") REFERENCES "public"."posts"("id","id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "comments_post_time_idx" ON "comments" USING btree ("post_id","created_at" DESC NULLS LAST,"id");--> statement-breakpoint
 CREATE INDEX "comments_user_time_idx" ON "comments" USING btree ("user_id","created_at" DESC NULLS LAST,"id");
